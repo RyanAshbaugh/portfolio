989ac733625f6c75cf940f4bc55d8dc5010edcff
Ryan Ashbaugh <rashbaugh23@gmail.com>
2024-03-22 22:10:25 -0400

    start about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles'
+import { textVariant } from '../utils/motion'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+    </>
   )
 }
 
