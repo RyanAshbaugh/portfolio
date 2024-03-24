2bbf1c19b267d9173ae3f65f8aa61ce5423c04af
Ryan Ashbaugh <rashbaugh23@gmail.com>
2024-03-24 01:56:32 -0400

    started works section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { github } from '../assets';
+import { motion } from 'framer-motion';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { styles } from '../styles';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          My work
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Projects.
+        </h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
