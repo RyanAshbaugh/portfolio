bdc996be55ecaf938ff61226198349965cbae7d9
Ryan Ashbaugh <rashbaugh23@gmail.com>
2024-03-24 02:15:11 -0400

    got cards working well

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,37 @@
-import { github } from '../assets';
+import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
+
+import { github } from '../assets';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 import { styles } from '../styles';
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      test
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]" >
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+        </div>
+
+      </Tilt>
+    </motion.div>
+  )
+
+}
 
 const Works = () => {
   return (
@@ -16,6 +44,25 @@ const Works = () => {
           Projects.
         </h2>
       </motion.div>
+
+      <div className="w-full flex">
+        <motion.p 
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Blah, blah blah, projects
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            {...project}
+          />
+        ))}
+      </div>
     </>
   )
 }
