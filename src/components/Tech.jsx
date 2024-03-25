d996936e732ad316e7678494dac541784143c217
Ryan Ashbaugh <rashbaugh23@gmail.com>
2024-03-25 01:35:56 -0400

    solved warning

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,8 +5,8 @@ import { technologies } from '../constants';
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28">
+      {technologies.map((technology, index) => (
+        <div className="w-28 h-28" key={index}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
